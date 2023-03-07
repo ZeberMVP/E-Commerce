86d439fec4f98c2ef7413e9fdab038dcd57e015b
ZeberMVP <rubeen722@gmail.com>
2023-03-07 19:36:42 +0100

    add/submit order

--- client/src/components/Main/Cart/Cart.jsx
+++ client/src/components/Main/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteCart, decreaseQuantity, increaseQuantity } from "../../../redux";
-
+import axios from 'axios';
 
 const Cart = () => {
     const dispatch = useDispatch();
@@ -14,6 +14,12 @@ const Cart = () => {
     });
 
     const [sizes, setSizes] = useState(items.map(() => 40));
+    const [email, setEmail] = useState('');
+    const serverUrl = process.env.SERVER_URL || 'http://localhost:5000';
+
+    const api = axios.create({
+        baseURL: serverUrl
+    });
 
     const handleSizeChange = (index, newSize) => {
         if (newSize >= 37 && newSize <= 47) {
@@ -25,72 +31,101 @@ const Cart = () => {
         }
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const order = {
+            items: items,
+            sizes: sizes,
+            email: email,
+            total: TotalCart.toFixed(2),
+        };
+
+        api.post('/api/order', order)
+            .then(response => {
+                console.log('Success:', response.data);
+                alert("Your order has been received. Check your email to provide us your address and payment details");
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+    };
 
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Name</th>
-                    <th>Image</th>
-                    <th>Price</th>
-                    <th>Size</th>
-                    <th>Quantity</th>
-                    <th>Total Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {items.map((item, i) => {
-                    const size = sizes[i];
-                    return (
-                        <tr key={i} i={i}>
-                            <td><button style={{ cursor: "pointer" }} onClick={() => {
-                                dispatch(deleteCart(i))
-                            }}>X</button></td>
-                            <td>{item.product_name}</td>
-                            <td><img src={item.image} alt={item.product_name} style={{ width: '100px', height: '80px' }} /></td>
-                            <td>{item.price} $</td>
-                            <td>
-                                <button
-                                    style={{ margin: '2px', cursor: 'pointer' }}
-                                    onClick={() => handleSizeChange(i, size - 1)}
-                                >
-                                    -
-                                </button>
-                                <span>{size}</span>
-                                <button
-                                    style={{ margin: '2px', cursor: 'pointer' }}
-                                    onClick={() => handleSizeChange(i, size + 1)}
-                                >
-                                    +
-                                </button>
-                            </td>
-                            <td>
-                                <button style={{ margin: '2px', cursor: "pointer" }} onClick={() => {
-                                    dispatch(decreaseQuantity(i))
-                                }}>
-                                    -
-                                </button>
-                                <span>{item.quantity}</span>
-                                <button style={{ margin: '2px', cursor: "pointer" }} onClick={() => {
-                                    dispatch(increaseQuantity(i))
-                                }}>
-                                    +
-                                </button>
-                            </td>
-                            <td><b>{(item.quantity * item.price).toFixed(2)} $</b></td>
-                        </tr>
-                    )
-                })}
-                <tr>
-                    <td colSpan="6" style={{ textAlign: 'right' }}>Total Price:</td>
-                    <td><b>{TotalCart.toFixed(2)} $</b></td>
-                </tr>
-            </tbody>
-        </table>
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Image</th>
+                        <th>Price</th>
+                        <th>Size</th>
+                        <th>Quantity</th>
+                        <th>Total Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item, i) => {
+                        const size = sizes[i];
+                        return (
+                            <tr key={i} i={i}>
+                                <td><button style={{ cursor: "pointer" }} onClick={() => {
+                                    dispatch(deleteCart(i))
+                                }}>X</button></td>
+                                <td>{item.product_name}</td>
+                                <td><img src={item.image} alt={item.product_name} style={{ width: '100px', height: '80px' }} /></td>
+                                <td>{item.price} $</td>
+                                <td>
+                                    <button
+                                        style={{ margin: '2px', cursor: 'pointer' }}
+                                        onClick={() => handleSizeChange(i, size - 1)}
+                                    >
+                                        -
+                                    </button>
+                                    <span>{size}</span>
+                                    <button
+                                        style={{ margin: '2px', cursor: 'pointer' }}
+                                        onClick={() => handleSizeChange(i, size + 1)}
+                                    >
+                                        +
+                                    </button>
+                                </td>
+                                <td>
+                                    <button style={{ margin: '2px', cursor: "pointer" }} onClick={() => {
+                                        dispatch(decreaseQuantity(i))
+                                    }}>
+                                        -
+                                    </button>
+                                    <span>{item.quantity}</span>
+                                    <button style={{ margin: '2px', cursor: "pointer" }} onClick={() => {
+                                        dispatch(increaseQuantity(i))
+                                    }}>
+                                        +
+                                    </button>
+                                </td>
+                                <td><b>{(item.quantity * item.price).toFixed(2)} $</b></td>
+                            </tr>
+                        )
+                    })}
+                    <tr>
+                        <td colSpan="6" style={{ textAlign: 'right' }}>Total Price:</td>
+                        <td><b>{TotalCart.toFixed(2)} $</b></td>
+                    </tr>
+                </tbody>
+            </table>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
-
-
 
 export default Cart
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require('morgan');
 // Routes modules
 const productRouter = require('./routes/productRoutes');
 const providerRouter = require('./routes/providerRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,11 +17,10 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(morgan('dev'));
 
-
-
-// Routes:
+// Routes
 app.use('/api/product', productRouter);
 app.use('/api/provider', providerRouter);
+app.use('/api/order', orderRouter);
 
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder
